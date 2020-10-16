d14ec6714d8476a215c863fe91385fb8990a5e9e
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-16 18:36:32 -0300

    Connected app to github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,50 @@
-import React, {useState} from 'react';
+import React, {useState, FormEvent} from 'react';
 import {FiChevronRight} from 'react-icons/fi';
 import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
-import {Title, Form, Repositories} from './styles'
+import {Title, Form, Repositories} from './styles';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState("");
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
-    function handleAddRepository() {
-
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault();
+        const response = await api.get<Repository>(`/repos/${newRepo}`);
+        const repository = response.data;
+        setRepositories([...repositories, repository]);
+        setNewRepo("");
     }
     return (
         <>
             <img src={logo} alt="Github Explorer"/>
             <Title>Explore repositórios no Github</Title>
-            <Form action="">
+            <Form onSubmit={handleAddRepository}>
                 <input placeholder="Digite o nome do repositório" value={newRepo} onChange={e => setNewRepo(e.target.value)} />
                 <button type="submit">Pesquisar</button>
             </Form>
             <Repositories>
-                <a href="teste">
-                    <img src="https://avatars0.githubusercontent.com/u/67802605?s=460&u=a709d8bb80a43019b49737b767ed11e4028f742f&v=4" alt="Mateus Rizzo"/>
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
+                    <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                     <div>
-                        <strong>mateusrizzo/sunshine</strong>
-                        <p>A weather app made on React Native</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
                     <FiChevronRight size={20} />
                 </a>
+                ))}
             </Repositories>
         </>
     );
