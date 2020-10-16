37b7b037107a3411e6d3403e5680b5c0560bdb00
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-16 17:50:54 -0300

    styled repository list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {FiChevronRight} from 'react-icons/fi'
 
 import logo from '../../assets/logo.svg';
 
-import {Title, Form} from './styles'
+import {Title, Form, Repositories} from './styles'
 
 const Dashboard: React.FC = () => {
     return (
@@ -13,6 +14,36 @@ const Dashboard: React.FC = () => {
                 <input placeholder="Digite o nome do repositório"/>
                 <button type="submit">Pesquisar</button>
             </Form>
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars0.githubusercontent.com/u/67802605?s=460&u=a709d8bb80a43019b49737b767ed11e4028f742f&v=4" alt="Mateus Rizzo"/>
+                    <div>
+                        <strong>mateusrizzo/sunshine</strong>
+                        <p>A weather app made on React Native</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars0.githubusercontent.com/u/67802605?s=460&u=a709d8bb80a43019b49737b767ed11e4028f742f&v=4" alt="Mateus Rizzo"/>
+                    <div>
+                        <strong>mateusrizzo/sunshine</strong>
+                        <p>A weather app made on React Native</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars0.githubusercontent.com/u/67802605?s=460&u=a709d8bb80a43019b49737b767ed11e4028f742f&v=4" alt="Mateus Rizzo"/>
+                    <div>
+                        <strong>mateusrizzo/sunshine</strong>
+                        <p>A weather app made on React Native</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
         </>
     );
 }
