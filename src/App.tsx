0f18d8a09d444bb7cf4d1d71902e658a5cbfb846
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-16 18:39:05 -0300

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Title } from './pages/Dashboard/styles';
 
 import Routes from './routes';
 import GlobalStyle from './styles/global';
