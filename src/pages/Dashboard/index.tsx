2412107777002d741efadf9c65bc76d6002ac15b
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-19 17:47:07 -0300

    Error treatment added

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
-import {Title, Form, Repositories} from './styles';
+import {Title, Form, Repositories, Error} from './styles';
 
 interface Repository {
     full_name: string;
@@ -17,14 +17,23 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState("");
+    const [inputError, setInputError] = useState("");
     const [repositories, setRepositories] = useState<Repository[]>([]);
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
-        const response = await api.get<Repository>(`/repos/${newRepo}`);
-        const repository = response.data;
-        setRepositories([...repositories, repository]);
-        setNewRepo("");
+        if (!newRepo){
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+        try{
+            const response = await api.get<Repository>(`/repos/${newRepo}`);
+            const repository = response.data;
+            setRepositories([...repositories, repository]);
+            setNewRepo("");
+        } catch (err){
+            setInputError("Erro na busca por esse repositório");
+        }
     }
     return (
         <>
@@ -34,6 +43,7 @@ const Dashboard: React.FC = () => {
                 <input placeholder="Digite o nome do repositório" value={newRepo} onChange={e => setNewRepo(e.target.value)} />
                 <button type="submit">Pesquisar</button>
             </Form>
+            {inputError && <Error>{inputError}</Error>}
             <Repositories>
                 {repositories.map(repository => (
                     <a key={repository.full_name} href="teste">
